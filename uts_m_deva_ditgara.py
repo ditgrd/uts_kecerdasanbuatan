e031fb781a28505b21b3faf7b94cc5e9c90be3a4
ditgrd <99669370+ditgrd@users.noreply.github.com>
2022-04-22 21:12:06 +0700

    Add files via upload

---
+++ uts_m_deva_ditgara.py
@@ -0,0 +1,17 @@
+def color_translator(color):
+	if color == "red":
+		hex_color = "#ff0000"
+	elif color == "green":
+		hex_color = "#00ff00"
+	elif color == "blue":
+		hex_color = "#0000ff"
+	else:
+		hex_color = "unknown"
+	return hex_color
+
+print(color_translator("blue"))
+print(color_translator("yellow"))
+print(color_translator("red"))
+print(color_translator("black"))
+print(color_translator("green"))
+print(color_translator(""))
